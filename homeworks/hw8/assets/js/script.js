3fdc432736cc33bd12747e218071b8c213061261
choi-joshua <choi.jh.joshua@gmail.com>
2023-04-12 16:30:24 -0700

    finished hw8

---
+++ homeworks/hw8/assets/js/script.js
@@ -0,0 +1,8 @@
+songsSecondHalf = document.getElementById("songs-second-half");
+songsSecondHalf.classList.add("hidden");
+
+function toggleSongs() {
+    songsSecondHalf.classList.toggle("hidden")
+}
+
+document.getElementById("toggle-button").onclick = toggleSongs;
